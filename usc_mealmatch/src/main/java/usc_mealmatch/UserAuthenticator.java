ea0b06d6301930e8367cb2237f2fa4f1a8f9c67d
Eugenia Druzhinina <86218340+genia-dru@users.noreply.github.com>
2023-04-06 14:27:57 -0700

    reuploaded files

---
+++ usc_mealmatch/src/main/java/usc_mealmatch/UserAuthenticator.java
@@ -0,0 +1,10 @@
+package usc_mealmatch;
+
+public class UserAuthenticator {
+	public boolean login(String email, String password) { //authenticate the user's login input
+		return true;
+	}
+	public boolean signup(String email, String password) { //authenticate the user's signup input
+		return true;
+	}
+}
