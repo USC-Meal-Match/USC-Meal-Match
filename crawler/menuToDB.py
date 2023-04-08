01f6ce2fb38f9d59aab82bb1fe86cc4e8ccfa69c
Eugenia Druzhinina <86218340+genia-dru@users.noreply.github.com>
2023-04-07 20:53:22 -0700

    Add files via upload

--- crawler/menuToDB.py
+++ crawler/menuToDB.py
@@ -13,27 +13,26 @@ try:
 	
 	menus = []
 	
-	sql_select = "SELECT * from dining_halls"
 	cursor = connection.cursor()
-	cursor.execute(sql_select) #select the dining_halls table
+	cursor.execute("TRUNCATE TABLE menu") #delete the pre-existing menu
+	cursor.execute("SELECT * from dining_halls") #select the dining_halls table
 	
 	diningHalls = cursor.fetchall() #retrieve the dining_halls table
 	
 	for row in diningHalls: #go through all the dining halls
 		diningHallID = row[0] #gets the ID of the dining hall
-		diningHallName = row[1] #gets the name of the dining hall
-		if(diningHallName == "parkside"):
+		diningHallName = row[1].lower() #gets the name of the dining hall
+		if diningHallName == "parkside": #set the number for the dining hall crawler
 			diningHallName = 1
-		elif(diningHallName == "village"):
+		elif diningHallName == "village":
 			diningHallName = 2
-		elif(diningHallName == "evk"):
+		elif diningHallName == "evk":
 			diningHallName = 3
 		for i in range(2): #crawl through each dining hall's session
 			menus = crawl_menus(i+1, diningHallName)
-			for j in menus: #input the item name, dining hall ID, and diet restrictions
-				sql_insert = """INSERT INTO menu(item_name, dining_hall_id, diet_restrictions)
-					VALUES (""" + menus[j][0] + """, """ + diningHallID + """, """ + menus[j][1] + """);""" 
-			cursor.execute(sql_insert)	
+			for j in range(len(menus)): #input the item name, dining hall ID, and diet restrictions
+				cursor.execute("""INSERT INTO menu(item_name, dining_hall_id, diet_restrictions)
+					VALUES (\"""" + menus[j]["name"] + """\", """ + str(diningHallID) + """, \"""" + "".join(menus[j]["allergy"]) + """\");""")	
 	connection.commit()
 	cursor.close()
 	
