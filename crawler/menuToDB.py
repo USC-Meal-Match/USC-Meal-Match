09efee3345c25b86fb0be9bdaad89fe4294fb627
Eugenia Druzhinina <86218340+genia-dru@users.noreply.github.com>
2023-04-11 15:39:39 -0700

    fully connects to cloud, fixed allergen separators

--- crawler/menuToDB.py
+++ crawler/menuToDB.py
@@ -11,28 +11,24 @@ try:
 										user='root',
 										password='csci201') #connects to the database
 	
-	menus = []
-	
-	cursor = connection.cursor()
+	cursor = connection.cursor() #connect the cursor
+
 	cursor.execute("TRUNCATE TABLE menu") #delete the pre-existing menu
 	cursor.execute("SELECT * from dining_halls") #select the dining_halls table
 	
 	diningHalls = cursor.fetchall() #retrieve the dining_halls table
+	query = "INSERT INTO menu(item_name, dining_hall_id, diet_restrictions) VALUES (%s, %s, %s)" #create the query for prepared statement
 	
 	for row in diningHalls: #go through all the dining halls
 		diningHallID = row[0] #gets the ID of the dining hall
-		diningHallName = row[1].lower() #gets the name of the dining hall
-		if diningHallName == "parkside": #set the number for the dining hall crawler
-			diningHallName = 1
-		elif diningHallName == "village":
-			diningHallName = 2
-		elif diningHallName == "evk":
-			diningHallName = 3
 		for i in range(2): #crawl through each dining hall's session
-			menus = crawl_menus(i+1, diningHallName)
+			menus = crawl_menus(i+1, diningHallID) #crawl the menu
 			for j in range(len(menus)): #input the item name, dining hall ID, and diet restrictions
-				cursor.execute("""INSERT INTO menu(item_name, dining_hall_id, diet_restrictions)
-					VALUES (\"""" + menus[j]["name"] + """\", """ + str(diningHallID) + """, \"""" + "".join(menus[j]["allergy"]) + """\");""")	
+				item_name = str(menus[j]["name"])
+				print(menus[j]["allergy"])
+				diet_restrictions = str(", ".join(menus[j]["allergy"]))
+				
+				cursor.execute(query, (item_name, str(diningHallID), diet_restrictions)) #execute prepared statement
 	connection.commit()
 	cursor.close()
 	
